e15d8334b218e63a7e783a1e8addc0ecd1a8239f
sulekhshende <92660699+sulekhshende@users.noreply.github.com>
2025-02-06 20:19:41 +0530

    Update AddNews.tsx

--- src/components/AddNews.tsx
+++ src/components/AddNews.tsx
@@ -215,7 +215,7 @@ export default function AddNews({  setShowNavbar }: AddNewsProps) {
         let bodys: any[] = []
 
         values.tags.forEach((tag)=>{
-            let url = 'https://ec2-3-109-255-100.ap-south-1.compute.amazonaws.com:7700/api/news_tags/'
+            let url = 'http://ec2-3-109-255-100:7700/api/news_tags/'
             endpoints.push(url)
             let body = {
                 news_id : news_id,
